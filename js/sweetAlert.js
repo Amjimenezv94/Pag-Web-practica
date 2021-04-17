1410462d5cc6a337853fa12abd7a6d742754126c
Angélica Jimenez Villa <amjimenezv@gmail.com>
2021-04-17 01:18:54 -0500

    first commit

---
+++ js/sweetAlert.js
@@ -0,0 +1,23 @@
+
+function btnsend(){
+    
+  swal("Sent!", "We have received your information correctly!", "success"
+    
+  
+   );
+ 
+}
+
+function btndelete(){
+   
+  swal("Are you sure?", {
+    dangerMode: true,
+    buttons: true,
+  });
+    }
+
+    function start(){
+   
+    
+      swal("Bienvenid@! porfavor llena el formulario.");
+        }
